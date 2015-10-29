9266469ed2e3cb347650155adfc5d887bc191636
Eric Myhre <hash@exultant.us>
2015-10-29 15:57:54 -0500

    extract a little more structurally

--- main.go
+++ main.go
@@ -1,43 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
+	"os"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func ExampleScrape() {
+type Vitalstats struct {
+	Name        string
+	Link        string
+	Power       string
+	ReleaseDate string
+	Type        string // e.g. "tablet"
+	CMSupport   string
+}
+
+func main() {
 	doc, err := goquery.NewDocument("http://wiki.cyanogenmod.org/w/Devices")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	doc.Find("span.device").Each(func(i int, s *goquery.Selection) {
-		deviceName := s.Find("span.name").Text()
-		deviceLink := s.Find("a").AttrOr("href", "")
-		fmt.Printf("Device %d: %q >> %s", i, deviceName, deviceLink)
+		vitals := Vitalstats{}
+
+		vitals.Name = s.Find("span.name").Text()
+		vitals.Link = s.Find("a").AttrOr("href", "")
 
 		// lol they implode if you strcat and get a "//" after the tld
-		doc2, err := goquery.NewDocument("http://wiki.cyanogenmod.org" + deviceLink)
+		doc2, err := goquery.NewDocument("http://wiki.cyanogenmod.org" + vitals.Link)
 		if err != nil {
 			log.Fatal(err)
 		}
 		doc2.Find("div#mw-content-text table tr").Each(func(_ int, s *goquery.Selection) {
-			//maul, _ := s.Html()
-			//fmt.Printf(">>>>     %s    ", maul)
-			if strings.TrimSpace(s.Find("th").Text()) != "Power:" {
-				//fmt.Printf(" :: %q", s.Find("th").Text())
-				return
+			switch strings.TrimSpace(s.Find("th").Text()) {
+			case "Power:":
+				vitals.Power = s.Find("td").Text()
+			case "Release Date:":
+				vitals.ReleaseDate = s.Find("td").Text()
+			case "Type:":
+				vitals.Type = s.Find("td").Text()
+			case "CM Support:":
+				vitals.CMSupport = s.Find("td").Text()
 			}
-			powerDesc := s.Find("td").Text()
-			fmt.Printf(" ... %q", powerDesc)
 		})
-		fmt.Print("\n")
+		json.NewEncoder(os.Stdout).Encode(vitals)
 	})
-}
 
-func main() {
-	ExampleScrape()
+	// remove:
+	//  - 'non-removable'
 }
